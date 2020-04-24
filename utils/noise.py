a36c94089f5db301b02005f2e1a1f8ff3e6281af
Komei Takabayashi <komei0727@gmail.com>
2020-04-25 03:46:10 +0900

    1枚の画像について想起完了、make_noiseをモジュール化

---
+++ utils/noise.py
@@ -0,0 +1,13 @@
+def make_noise(q, noise):
+    import numpy as np
+    q_noise = np.zeros((1,25), dtype = int)
+    for i in range(len(q[0])):
+        rand = np.random.randint(100)
+        if rand < noise:
+            if q[0][i] == 1:
+                q_noise[0][i] = -1
+            else:
+                q_noise[0][i] = 1
+        else:
+            q_noise[0][i] = q[0][i]
+    return q_noise
