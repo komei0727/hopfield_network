9edd8d990e300ae4b2f778317d660aaffb8c83ee
Komei Takabayashi <komei0727@gmail.com>
2020-06-01 14:12:51 +0900

    １種類の画像の想起を行うプログラムの完成

--- single_picture.py
+++ single_picture.py
@@ -1,9 +1,13 @@
 import numpy as np
 from utils.noise import make_noise
+from utils.recollection import recollection
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 #ノイズと閾値の設定
 noise = 20
 thete = 0
+N = 100
 
 #画像の読み込み
 Q_1 = np.loadtxt("picture/picture_1.csv", delimiter = ",", dtype = int)
@@ -20,11 +24,26 @@ np.putmask(W_diag, W_diag > 0, 0)
 #元画像にノイズを加える
 q_1_noise = make_noise(q_1, noise)
 
-for n in range(100):
-    similar = np.dot(q_1_noise[0], q_1[0]) / 25
-    print(similar)
-    i = np.random.randint(25)
-    if np.dot(W[i], q_1_noise[0]) - thete >= 0:
-        q_1_noise[0][i] = 1
-    else:
-        q_1_noise[0][i] = -1
+#画像を想起する
+similar, q_1_rec = recollection(q_1[0], q_1_noise[0], W, thete, N)
+
+#結果のグラフを表示する
+Q_1_noise = q_1_noise.reshape(5,5)
+Q_1_rec = q_1_rec.reshape(5,5)
+x = np.linspace(1,100,100)
+fig = plt.figure(figsize = (12,8))
+gs = gridspec.GridSpec(2,3)
+ax1 = fig.add_subplot(gs[0,0])
+ax1.set_title('input')
+ax1.imshow(Q_1, cmap="gray")
+ax2 = fig.add_subplot(gs[0,1])
+ax2.set_title('with noise')
+ax2.imshow(Q_1_noise, cmap="gray")
+ax3 = fig.add_subplot(gs[0,2])
+ax3.set_title('output')
+ax3.imshow(Q_1_rec, cmap="gray")
+ax4 = fig.add_subplot(gs[1,:])
+ax4.set_title('degree of similarity')
+ax4.plot(x, similar)
+
+plt.show()
--- utils/recollection.py
+++ utils/recollection.py
@@ -1,12 +1,14 @@
 def recollection(q, q_noise, W, thete, N):
     import numpy as np
+    import copy
     S = np.zeros(100)
+    q_rec = copy.copy(q_noise)
     for n in range(N): 
-        S[n] = np.dot(q_noise, q) / 25
+        S[n] = np.dot(q_rec, q) / 25
         i = np.random.randint(25)
-        if np.dot(W[i], q_noise) - thete >= 0:
-            q_noise[i] = 1
+        if np.dot(W[i], q_rec) - thete >= 0:
+            q_rec[i] = 1
         else:
-            q_noise[i] = -1
+            q_rec[i] = -1
 
-    return S, q_noise
+    return S, q_rec
